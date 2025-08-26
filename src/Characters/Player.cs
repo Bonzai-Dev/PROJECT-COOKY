a9deb5cf46bad4e668ca776f8800abf32c6e554a
Bonzai-Dev <137844567+Bonzai-Dev@users.noreply.github.com>
2025-08-26 15:29:05 -0400

    Added grabbing

--- src/Characters/Player.cs
+++ src/Characters/Player.cs
@@ -4,13 +4,20 @@ using PhantomCamera;
 public partial class Player : Character {
   [Export] private Node3D Camera;
   [Export] private float MouseSensitivity = 0.1f;
-
+  [Export] private float GrabRange = 5f;
+  [Export] private bool FreecamEnabled;
+  
+  private Node3D head;
+  private RayCast3D lookatRaycast;
   private Vector2 mouseDelta;
   private Vector2 mouseRotation;
-  [Export] private bool FreecamEnabled = true;
+  private RigidBody3D grabbedItem;
 
   public override void _Ready() {
     Input.SetMouseMode(Input.MouseModeEnum.Captured);
+    head = GetNode<Node3D>("Head");
+    lookatRaycast = head.GetNode<RayCast3D>("Lookat");
+    lookatRaycast.AddException(this);
   }
 
   public override void _Input(InputEvent @event) {
@@ -18,6 +25,12 @@ public partial class Player : Character {
       mouseDelta = mouseEvent.ScreenRelative * MouseSensitivity * (float)GetProcessDeltaTime();
     if (Input.IsActionJustPressed("movement_jump") && OnGround())
       moveDirection = new Vector3(moveDirection.X, JumpForce, moveDirection.Y);
+    if (Input.IsActionJustPressed("pickup")) {
+      if (grabbedItem == null)
+        PickupItem(ItemInSight());
+      else
+        DropItem();
+    }
   }
 
   public override void _Process(double delta) {
@@ -33,7 +46,7 @@ public partial class Player : Character {
       forwardDirection * Input.GetAxis("movement_forward", "movement_backward")
     ).Normalized();
     
-    Camera.AsPhantomCamera3D().FollowTarget = (Node3D)FindChild("Camera");
+    Camera.AsPhantomCamera3D().FollowTarget = head;
     if (FreecamEnabled) {
       moveDirection = (
         moveDirection + Camera.GlobalTransform.Basis.Z.Normalized() *
@@ -48,22 +61,33 @@ public partial class Player : Character {
     
     Camera.Rotation = new Vector3(Mathf.Clamp(mouseRotation.X, -Mathf.Pi / 2, Mathf.Pi / 2), mouseRotation.Y, 0);
     Model.Rotation = new Vector3(0, mouseRotation.Y, 0);
+    head.Rotation = Camera.Rotation;
+    lookatRaycast.TargetPosition = new Vector3(0, GrabRange, 0);
     mouseDelta = Vector2.Zero;
   }
 
   public override void _PhysicsProcess(double delta) {
-    var slideDirection = SlideDirection();
-    var forceDirection = (moveDirection + slideDirection).Normalized();
+    if (FreecamEnabled) return;
 
-    if (!FreecamEnabled) {
-      ApplyForce(forceDirection * Acceleration + new Vector3(0, moveDirection.Y * JumpForce, 0));
+    if (grabbedItem != null) {
+      var rayEnd = lookatRaycast.ToGlobal(lookatRaycast.TargetPosition);
+      grabbedItem.SetLinearVelocity((rayEnd - grabbedItem.GlobalTransform.Origin) * 8);
     }
     
-    // Logic for when the player is in the air
+    var slideDirection = SlideDirection();
+    var forceDirection = (moveDirection + slideDirection).Normalized();
+    var appliedForce = new Vector3(
+      forceDirection.X * Acceleration, 0, forceDirection.Z * Acceleration
+    );
+    
+    ApplyForce(appliedForce);
+    
     if (!OnGround())
       ApplyForce(new Vector3(-moveDirection.X, 0, -moveDirection.Z) * CounterForce);
-    else
+    else {
+      ApplyForce(new Vector3(0, moveDirection.Y * JumpForce, 0));
       ApplyForce(new Vector3(-LinearVelocity.X * Deceleration, 0, -LinearVelocity.Z * Deceleration));
+    }
     
     // Logic for adding friction to player to prevent sliding
     if (Moving())
@@ -76,5 +100,29 @@ public partial class Player : Character {
       var normalizedVelocity = LinearVelocity.Normalized();
       LinearVelocity = new Vector3(normalizedVelocity.X * Speed, LinearVelocity.Y, normalizedVelocity.Z * Speed);
     }
+  }
+
+  private void PickupItem(RigidBody3D item) {
+    if (item == null) return;
+    grabbedItem = item;
+    grabbedItem.Rotation = new Vector3(0, Camera.Rotation.Y, 0);
+    grabbedItem.AxisLockAngularX = true;
+    grabbedItem.AxisLockAngularY = true;
+    grabbedItem.AxisLockAngularZ = true;
+    GD.Print("Player grabbed ", item.Name);
+  }
+
+  private void DropItem() {
+    GD.Print("Player dropped ", grabbedItem.Name);
+    grabbedItem.AxisLockAngularX = false;
+    grabbedItem.AxisLockAngularY = false;
+    grabbedItem.AxisLockAngularZ = false;
+    grabbedItem = null;
+  }
+  
+  // Simple raycast check as all the pickable objects are rigidbodies
+  private RigidBody3D ItemInSight() {
+    if (lookatRaycast.GetCollider() is RigidBody3D item) return item;
+    return null;
   }
 }

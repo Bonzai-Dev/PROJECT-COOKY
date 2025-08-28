041a816c468e6dcbf7889fae28bf302e6e162e7b
Bonzai-Dev <137844567+Bonzai-Dev@users.noreply.github.com>
2025-08-28 11:12:42 -0400

    VHS effect

--- src/Game.cs
+++ src/Game.cs
@@ -1,17 +1,23 @@
 using Godot;
 
 public partial class Game : Node {
-  private static CanvasLayer userInterface;
+  private static Control userInterface;
+  private static CanvasLayer hud;
   private static ColorRect fadeScreen;
+  private static CanvasModulate fadePointer;
   private static Tween fadeScreenTween;
+  private static Tween hudTween;
   
   public override void _Ready() {
-    userInterface = GetTree().CurrentScene.GetNode<CanvasLayer>("UI");
-    fadeScreen = userInterface.GetNode<ColorRect>("Fade");
+    userInterface = GetTree().CurrentScene.GetNode<Control>("UI");
+    hud = userInterface.GetNode<CanvasLayer>("Hud");
+    fadeScreen = hud.GetNode<ColorRect>("Fade/Fade");
+    fadePointer = hud.GetNode<CanvasModulate>("Pointer/Fade");
     fadeScreenTween = GetTree().CreateTween().BindNode(fadeScreen).SetTrans(Tween.TransitionType.Quint);
+    hudTween = GetTree().CreateTween().BindNode(fadePointer).SetTrans(Tween.TransitionType.Quint);
   }
 
-  public static CanvasLayer GetUserInterface() {
+  public static Control GetUserInterface() {
     return userInterface;
   }
   
@@ -19,11 +25,17 @@ public partial class Game : Node {
     fadeScreenTween.TweenProperty(
       fadeScreen, "color", new Color(0, 0, 0, 1), 1.0f
     );
+    hudTween.TweenProperty(
+      fadePointer, "color", new Color(0, 0, 0, 1), 1.0f
+    );
   }
 
   public static void FadeOut() {
     fadeScreenTween.TweenProperty(
       fadeScreen, "color", new Color(0, 0, 0, 0), 1.0f
+    );
+    hudTween.TweenProperty(
+      fadePointer, "color", new Color(255, 255, 255), 1.0f
     );
   }
 }
